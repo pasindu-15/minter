6f3ee5979c941e7fcce3d3c40b8da110afe2086d
Pasindu <hewagamagepasindu@gmail.com>
2022-11-15 23:12:52 +0530

    initial code

---
+++ src/test/java/com/msc/research/minter/MinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.research.minter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
